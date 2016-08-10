bd7c236b4e4f3c977fb07bf797c902a18a80221e
ernilambar <nilambar@outlook.com>
2016-08-10 16:27:26 +0545

    Add check for plugin

--- commands/wpbeta.php
+++ commands/wpbeta.php
@@ -33,6 +33,12 @@ class Devtools_WPBeta_Command extends WP_CLI_Command {
 		if ( ! in_array( $mode, array( 'bleeding', 'point' ) ) ) {
 			WP_CLI::error( 'Invalid mode.' );
 		}
+
+		$beta_tester = $this->is_beta_tester_available();
+		if ( is_wp_error( $beta_tester ) ) {
+			WP_CLI::error( $beta_tester );
+		}
+
 		$value = ( 'point' === $mode ) ? 'point' : 'unstable';
 
 		WP_CLI::launch_self( 'option set', array( 'wp_beta_tester_stream', $value ), array(), false, true );
@@ -40,6 +46,28 @@ class Devtools_WPBeta_Command extends WP_CLI_Command {
 		$success_message = "Mode set to '%s'.";
 		WP_CLI::success( sprintf( $success_message, $this->modes[ $value ] ) );
 
+	}
+
+	/**
+	 * Is the requested beta tester available?
+	 */
+	private function is_beta_tester_available() {
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+		if ( class_exists( 'wp_beta_tester' ) ) {
+			return true;
+		}
+
+		$plugins = get_plugins();
+		$beta_tester = 'wordpress-beta-tester/wp-beta-tester.php';
+		if ( array_key_exists( $beta_tester, $plugins ) ) {
+			$error_msg = "WordPress Beta Tester needs to be activated. Try 'wp plugin activate wordpress-beta-tester'.";
+		}
+		else {
+			$error_msg = "WordPress Beta Tester needs to be installed. Try 'wp plugin install wordpress-beta-tester --activate'.";
+		}
+
+		return new WP_Error( 'beta-tester-missing', $error_msg );
 	}
 }
 
--- features/wpbeta.feature
+++ features/wpbeta.feature
@@ -8,3 +8,19 @@ Feature: Test WordPress Beta Tester.
       """
       Success: Mode set to 'Bleeding edge nightlies'.
       """
+    And I run `wp option get wp_beta_tester_stream`
+    Then STDOUT should be:
+      """
+      unstable
+      """
+
+    When I run `wp dt wpbeta point`
+    Then STDOUT should be:
+      """
+      Success: Mode set to 'Point release nightlies'.
+      """
+    And I run `wp option get wp_beta_tester_stream`
+    Then STDOUT should be:
+      """
+      point
+      """
