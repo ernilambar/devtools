c5d0d0c0d8946d55d04e67c17aebebdeb53802c2
Nilambar Sharma <nilambar@outlook.com>
2016-08-13 10:37:39 +0545

    List extra image sizes

--- commands/image.php
+++ commands/image.php
@@ -4,22 +4,59 @@
  */
 class Devtools_Image_Command {
 
+	private $fields = array(
+		'id',
+		'width',
+		'height',
+		'crop',
+	);
+
 	/**
 	 * Image info.
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     # Set front page display to latest posts.
-	 *     $ wp dt front post
-	 *     Success: Front page displays set to Latest Posts.
-	 *
-	 *     # Set front page display to static page.
-	 *     $ wp dt front page
-	 *     Success: Front page displays set to Static Page.
+	 *     # List image sizes and info.
+	 *     $ wp dt image info
+	 *     +----------------+-------+--------+------+
+	 *     | id             | width | height | crop |
+	 *     +----------------+-------+--------+------+
+	 *     | post-thumbnail | 1200  | 9999   |      |
+	 *     +----------------+-------+--------+------+
 	 */
 	public function info( $args, $assoc_args ) {
 
-		print_r( 'Image info here' );
+		$image_info_detail = $this->get_image_info_detail();
+
+		if ( ! empty( $assoc_args['format'] ) && 'ids' === $assoc_args['format'] ) {
+			$image_info = wp_list_pluck( $image_info_detail, 'id' );
+		}
+		else {
+			$image_info = $image_info_detail;
+		}
+
+		$formatter = new \WP_CLI\Formatter( $assoc_args, $this->fields );
+		$formatter->display_items( $image_info );
+
+	}
+
+	private function get_image_info_detail() {
+
+		$output = array();
+		global $_wp_additional_image_sizes;
+
+		if ( ! empty( $_wp_additional_image_sizes ) ) {
+			foreach ($_wp_additional_image_sizes as $key => $val ) {
+				$item           = array();
+				$item['id']     = $key;
+				$item['width']  = $val['width'];
+				$item['height'] = $val['height'];
+				$item['crop']   = $val['crop'];
+				$output[ $key ] = $item;
+			}
+		}
+
+		return $output;
 
 	}
 
